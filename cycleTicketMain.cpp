988322f9db70f54e1e51f7194a480532f8202716
Htet Aung Khant <112607740+HtetAungKhant23@users.noreply.github.com>
2022-11-10 14:55:02 +0630

    Create cycleTicketMain.cpp
    version1

---
+++ cycleTicketMain.cpp
@@ -0,0 +1,16 @@
+//
+// Created by User on 11/10/2022.
+//
+// version 1
+
+#include "iostream"
+#include "cycleTicketForNCC.h"
+using namespace std;
+using namespace CYCLE;
+
+int main(){
+    CycleTicketForNCC obj;
+    obj.welcome();
+
+    return 0;
+}
