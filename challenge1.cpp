eb62a67fc4e0c8a15618de0ddc1ef382810f086c
Htet Aung Khant <112607740+HtetAungKhant23@users.noreply.github.com>
2022-11-14 08:35:59 +0630

    Update challenge1.cpp
    final

--- challenge1.cpp
+++ challenge1.cpp
@@ -3,24 +3,28 @@
 //
 
 // generate random character and write to file and then choose their respective character and replace it all.
-// version1
+// version1 
+// version2 (final and a little different from question, but I tried my best)
 
 #include "iostream"
-#include "stdlib.h"
+//#include "stdlib.h"
 #include "fstream"
 using namespace std;
 
 int main(){
 
-    string tempArr[10];
+    string tempArr[26];
+    int largestNum[26];
 
 
 // random character generate
     char arr[1000];
-    srand(time(0));
+    char randChar;
+    srand(time(NULL));
     for(int i=0; i<1000; i++) {
- //       cout << "random lowercase = " << char('a' + rand() % 26) << endl;
-        arr[i]=char('a' + rand() % 26);
+//        cout << "random lowercase = " << char('a' + rand() % 26) << endl;
+        randChar = 97 + (rand() % 26);
+        arr[i]=randChar;
     }
 
 
@@ -51,15 +55,55 @@ int main(){
     string f;
     string g;
     string h;
-    string i;
+    string j;
+    string jj;
+    string k;
+    string l;
+    string m;
+    string n;
+    string o;
+    string p;
+    string q;
+    string r;
+    string s;
+    string t;
+    string u;
+    string v;
+    string w;
+    string x;
+    string y;
+    string z;
 
-    int count=0;
+    int count;
     int aNum=0;
     int bNum=0;
     int cNum=0;
+    int dNum=0;
+    int eNum=0;
+    int fNum=0;
+    int gNum=0;
+    int hNum=0;
+    int iNum=0;
+    int jNum=0;
+    int kNum=0;
+    int lNum=0;
+    int mNum=0;
+    int nNum=0;
+    int oNum=0;
+    int pNum=0;
+    int qNum=0;
+    int rNum=0;
+    int sNum=0;
+    int tNum=0;
+    int uNum=0;
+    int vNum=0;
+    int wNum=0;
+    int xNum=0;
+    int yNum=0;
+    int zNum=0;
 
 
-    int largestNum[3];
+
 
 
 
@@ -73,8 +117,10 @@ int main(){
             for(auto &ch: dataLine){
                 if(ch==' '){
                     if(data=="a"){
+                        count=0;
                         aNum++;
-                        a=a+data+"\n";
+//                        a=a+data+"\n";
+                        a+=data;
                         cout<<"char a is = "<<data<<endl;
                         data="";
                         largestNum[count]=aNum;
@@ -83,7 +129,7 @@ int main(){
                         count=1;
                         bNum++;
                         cout<<"char b is = "<<data<<endl;
-                        b=b+data;
+                        b+=data;
                         data="";
                         largestNum[count]=bNum;
 
@@ -91,39 +137,193 @@ int main(){
                         count=2;
                         cNum++;
                         cout<<"char c is = "<<data<<endl;
-                        c=c+data;
+                        c+=data;
                         data="";
                         largestNum[count]=cNum;
 
                     }else if(data=="d"){
+                        count=3;
+                        dNum++;
                         cout<<"char d is = "<<data<<endl;
-                        d=d+data;
+                        d+=data;
                         data="";
+                        largestNum[count]=dNum;
 
                     }else if(data=="e"){
+                        count=4;
+                        eNum++;
                         cout<<"char e is = "<<data<<endl;
-                        e=e+data;
+                        e+=data;
                         data="";
+                        largestNum[count]=eNum;
 
                     }else if(data=="f"){
+                        count=5;
+                        fNum++;
                         cout<<"char f is = "<<data<<endl;
-                        f=f+data;
+                        f+=data;
                         data="";
+                        largestNum[count]=fNum;
 
                     }else if(data=="g"){
+                        count=6;
+                        gNum++;
                         cout<<"char g is = "<<data<<endl;
-                        g=g+data;
+                        g+=data;
                         data="";
+                        largestNum[count]=gNum;
 
                     }else if(data=="h"){
+                        count=7;
+                        hNum++;
                         cout<<"char h is = "<<data<<endl;
-                        h=h+data;
+                        h+=data;
                         data="";
+                        largestNum[count]=hNum;
 
                     }else if(data=="i"){
+                        count=8;
+                        iNum++;
                         cout<<"char i is = "<<data<<endl;
-                        i=i+data;
+                        j+=data;
                         data="";
+                        largestNum[count]=iNum;
+
+                    }else if(data=="j"){
+                        count=9;
+                        jNum++;
+                        cout<<"char j is = "<<data<<endl;
+                        jj+=data;
+                        data="";
+                        largestNum[count]=jNum;
+
+                    }else if(data=="k"){
+                        count=10;
+                        kNum++;
+                        cout<<"char k is = "<<data<<endl;
+                        k+=data;
+                        data="";
+                        largestNum[count]=kNum;
+
+                    }else if(data=="l"){
+                        count=11;
+                        lNum++;
+                        cout<<"char l is = "<<data<<endl;
+                        l+=data;
+                        data="";
+                        largestNum[count]=lNum;
+
+                    }else if(data=="m"){
+                        count=12;
+                        mNum++;
+                        cout<<"char m is = "<<data<<endl;
+                        m+=data;
+                        data="";
+                        largestNum[count]=mNum;
+
+                    }else if(data=="n"){
+                        count=13;
+                        nNum++;
+                        cout<<"char n is = "<<data<<endl;
+                        n+=data;
+                        data="";
+                        largestNum[count]=nNum;
+
+                    }else if(data=="o"){
+                        count=14;
+                        oNum++;
+                        cout<<"char o is = "<<data<<endl;
+                        o+=data;
+                        data="";
+                        largestNum[count]=oNum;
+
+                    }else if(data=="p"){
+                        count=15;
+                        pNum++;
+                        cout<<"char p is = "<<data<<endl;
+                        p+=data;
+                        data="";
+                        largestNum[count]=pNum;
+
+                    }else if(data=="q"){
+                        count=16;
+                        qNum++;
+                        cout<<"char q is = "<<data<<endl;
+                        q+=data;
+                        data="";
+                        largestNum[count]=qNum;
+
+                    }else if(data=="r"){
+                        count=17;
+                        rNum++;
+                        cout<<"char r is = "<<data<<endl;
+                        r+=data;
+                        data="";
+                        largestNum[count]=rNum;
+
+                    }else if(data=="s"){
+                        count=18;
+                        sNum++;
+                        cout<<"char s is = "<<data<<endl;
+                        s+=data;
+                        data="";
+                        largestNum[count]=sNum;
+
+                    }else if(data=="t"){
+                        count=19;
+                        tNum++;
+                        cout<<"char t is = "<<data<<endl;
+                        t+=data;
+                        data="";
+                        largestNum[count]=tNum;
+
+                    }else if(data=="u"){
+                        count=20;
+                        uNum++;
+                        cout<<"char u is = "<<data<<endl;
+                        u+=data;
+                        data="";
+                        largestNum[count]=uNum;
+
+                    }else if(data=="v"){
+                        count=21;
+                        vNum++;
+                        cout<<"char v is = "<<data<<endl;
+                        v+=data;
+                        data="";
+                        largestNum[count]=vNum;
+
+                    }else if(data=="w"){
+                        count=22;
+                        wNum++;
+                        cout<<"char w is = "<<data<<endl;
+                        w+=data;
+                        data="";
+                        largestNum[count]=wNum;
+
+                    }else if(data=="x"){
+                        count=23;
+                        xNum++;
+                        cout<<"char x is = "<<data<<endl;
+                        x+=data;
+                        data="";
+                        largestNum[count]=xNum;
+
+                    }else if(data=="y"){
+                        count=24;
+                        yNum++;
+                        cout<<"char y is = "<<data<<endl;
+                        y+=data;
+                        data="";
+                        largestNum[count]=yNum;
+
+                    }else if(data=="z"){
+                        count=25;
+                        zNum++;
+                        cout<<"char z is = "<<data<<endl;
+                        z+=data;
+                        data="";
+                        largestNum[count]=zNum;
 
                     }else{
                         data="";
@@ -132,6 +332,7 @@ int main(){
                 else{
                     data = ch;
                 }
+
                 tempArr[0]=a;
                 tempArr[1]=b;
                 tempArr[2]=c;
@@ -140,7 +341,24 @@ int main(){
                 tempArr[5]=f;
                 tempArr[6]=g;
                 tempArr[7]=h;
-                tempArr[8]=i;
+                tempArr[8]=j;
+                tempArr[9]=jj;
+                tempArr[10]=k;
+                tempArr[11]=l;
+                tempArr[12]=m;
+                tempArr[13]=n;
+                tempArr[14]=o;
+                tempArr[15]=p;
+                tempArr[16]=q;
+                tempArr[17]=r;
+                tempArr[18]=s;
+                tempArr[19]=t;
+                tempArr[20]=u;
+                tempArr[21]=v;
+                tempArr[22]=w;
+                tempArr[23]=x;
+                tempArr[24]=y;
+                tempArr[25]=z;
 
             }
 
@@ -151,11 +369,14 @@ int main(){
     }
 
 
-    for(int i=0; i<3; i++){
-        if(largestNum[0]< largestNum[i+1]){
-            largestNum[0]= largestNum[i+1];
+    for(int i=0; i<=25; i++){
+        cout<<"Number array is = "<<largestNum[i]<<" ";
+        if(largestNum[0]< largestNum[i]){
+            largestNum[0]= largestNum[i];
         }
     }
+
+    cout<<"largest number is = "<<largestNum[0]<<endl;
 
 
     string mostResult;
@@ -165,12 +386,55 @@ int main(){
         mostResult='b';
     } else if(largestNum[0]==cNum){
         mostResult='c';
+    } else if(largestNum[0]==dNum){
+        mostResult='d';
+    } else if(largestNum[0]==eNum){
+        mostResult='e';
+    } else if(largestNum[0]==fNum){
+        mostResult='f';
+    } else if(largestNum[0]==gNum){
+        mostResult='g';
+    } else if(largestNum[0]==hNum){
+        mostResult='h';
+    } else if(largestNum[0]==iNum){
+        mostResult='i';
+    } else if(largestNum[0]==jNum){
+        mostResult='j';
+    } else if(largestNum[0]==kNum){
+        mostResult='k';
+    } else if(largestNum[0]==lNum){
+        mostResult='l';
+    } else if(largestNum[0]==mNum){
+        mostResult='m';
+    } else if(largestNum[0]==nNum){
+        mostResult='n';
+    } else if(largestNum[0]==oNum){
+        mostResult='o';
+    } else if(largestNum[0]==pNum){
+        mostResult='p';
+    } else if(largestNum[0]==qNum){
+        mostResult='q';
+    } else if(largestNum[0]==rNum){
+        mostResult='r';
+    } else if(largestNum[0]==sNum){
+        mostResult='s';
+    } else if(largestNum[0]==tNum){
+        mostResult='t';
+    } else if(largestNum[0]==uNum){
+        mostResult='u';
+    } else if(largestNum[0]==vNum){
+        mostResult='v';
+    } else if(largestNum[0]==wNum){
+        mostResult='w';
+    } else if(largestNum[0]==xNum){
+        mostResult='x';
+    } else if(largestNum[0]==yNum){
+        mostResult='y';
+    } else if(largestNum[0]==zNum){
+        mostResult='z';
     }
 
     cout<<"most result is = "<<mostResult<<endl;
-
-
-
 
 
 
@@ -183,13 +447,35 @@ int main(){
     finalCharFile.open(finalFile);
 
 
-    finalCharFile<<"A"<<"   "<<"B"<<"   "<<endl;
+ //   finalCharFile<<"A"<<"   "<<"B"<<"   "<<endl;
+
+    char cha;
+
+    for(int i=0; i<26; i++){
+
+        cha = (65+i);
+        finalCharFile<<cha<<"  =  ";
+        finalCharFile<<tempArr[i]<<endl;
+
+        cout << i+1 << " random char is = " << cha << endl;
+    }
+
+    finalCharFile<<endl;
 
 
 
+//    finalCharFile<<tempArr[0]<<"   "<<tempArr[1]<<"   "<<tempArr[2]<<"   "<<endl;
+//    finalCharFile<<a<<"    "<<b<<"    "<<c<<"    "<<d<<"    "<<e<<"    "<<f<<"    "<<g<<"    "<<h<<"    "<<j<<"    "<<endl;
 
-    finalCharFile<<tempArr[0]<<"   "<<tempArr[1]<<"   "<<tempArr[2]<<"   "<<endl;
-    finalCharFile<<"a = "<<aNum<<endl<<"b = "<<bNum<<endl<<"c = "<<cNum<<endl<<"most of char is = "<<largestNum[0]<<endl;
+//    for(int i=0; i<4; i++){
+//        finalCharFile<<tempArr[i];
+//    }
+
+
+    finalCharFile<<"a = "<<aNum<<endl<<"b = "<<bNum<<endl<<"c = "<<cNum<<endl<<"d = "<<dNum<<endl<<"e = "<<eNum<<endl<<"f = "<<fNum<<endl<<"g = "<<gNum<<endl<<"h = "<<hNum<<endl<<"i = "<<iNum<<endl;
+    finalCharFile<<"j = "<<jNum<<endl<<"k = "<<kNum<<endl<<"l = "<<lNum<<endl<<"m = "<<mNum<<endl<<"n = "<<nNum<<endl<<"o = "<<oNum<<endl<<"p = "<<pNum<<endl;
+    finalCharFile<<"q = "<<qNum<<endl<<"r = "<<rNum<<endl<<"s = "<<sNum<<endl<<"t = "<<tNum<<endl<<"u = "<<uNum<<endl<<"v = "<<vNum<<endl;
+    finalCharFile<<"w = "<<wNum<<endl<<"x = "<<xNum<<endl<<"y = "<<yNum<<endl<<"z = "<<zNum<<endl<<"most of char is = "<<mostResult<<"("<<largestNum[0]<<")"<<endl;
     finalCharFile.close();
 
 
